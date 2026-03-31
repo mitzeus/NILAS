9ccfbf04f76fcca3d2b401e2e4dccc6100b01ebd
WootKennziz - Mac <91803944+mitzeus@users.noreply.github.com>
2026-03-31 22:55:52 +0200

    almost finished spanish preprocessing pipeline

--- src/preprocessing/spanish.py
+++ src/preprocessing/spanish.py
@@ -1,9 +1,25 @@
+from pathlib import Path
+import os
+import requests
+import tarfile
+import shutil
+import json
+import csv
+import sqlite3
+import spacy
+import pandas as pd
+
+
 def load_dataset():
+    """Loads Spanish Dataset "ChatSubs" from source and extracts files inside
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     # Dataset does not fit locally so download from source
-    import os
-    import requests
-    import tarfile
-    import shutil
 
     def safe_filter(tarinfo, extract_path):
         # Reject absolute paths or parent directory traversal
@@ -40,4 +56,197 @@ def load_dataset():
     shutil.rmtree(os.path.join(spanish_raw_path, "open_subtitles_ca"))
     shutil.rmtree(os.path.join(spanish_raw_path, "open_subtitles_eu"))
     shutil.rmtree(os.path.join(spanish_raw_path, "open_subtitles_gl"))
+    os.remove(os.path.join(spanish_raw_path, "export.txt"))
     print("Spanish dataset downloaded and extracted successfully.")
+
+
+def extract_data_from_dataset(limit: int = float("inf")):
+    """Extracts data generated from `load_dataset` and combines all documents into a .csv file with columns: ID, dialogue.
+
+    Args:
+        limit (Optional, float(inf)): Limits how many docs are processed. Used for testing.
+
+    Returns:
+        None
+    """
+    spanish_root = "data/1-raw/spanish/"
+    spanish_target = "data/1-raw/"
+
+    doc_process_limit = limit
+
+    # extract data from each file
+    def extract_json_data(root_path):
+        root = Path(root_path)
+        total_files_to_process = min(
+            [len(list(root.rglob("*.jsonl"))), doc_process_limit]
+        )
+        for doc_i, doc in enumerate(root.rglob("*.jsonl")):
+            if doc_i >= doc_process_limit:
+                break
+
+            print(
+                f"({round((((doc_i+1)/total_files_to_process)) * 100, 2)}%) Processing Document {doc_i+1}/{total_files_to_process}",
+                doc,
+            )
+
+            with doc.open("r", encoding="utf-8") as f:
+                data = json.load(f)
+                yield data
+
+    def doc_preprocess(doc):
+        # here, doc is a dictionary: doc["dialogues"] extract dialogues and is a list of strings (each string is a dialogue)
+        for dialogue in doc["dialogues"]:
+            yield dialogue
+
+    # write all conversations to one document
+    with open(os.path.join(spanish_target, "spanish.csv"), "w", newline="") as csvfile:
+        fieldnames = ["ID", "dialogue"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        idx_counter = 0
+        for item in extract_json_data(spanish_root):
+            for extracted_dialogue in doc_preprocess(item):
+                clean_dialogue = extracted_dialogue.replace("\n", "\\n")
+                writer.writerows([{"ID": idx_counter, "dialogue": clean_dialogue}])
+                idx_counter += 1
+
+
+def grammar_preprocessing(
+    nlp_size: str = "large", cores_to_use: int = 1, chunk_size: int = 10000
+):
+    """Preprocesses .csv file generated from `extract_data_from_dataset` using spaCy.
+    It tokenizes, lemmatizes, and PoS tags all words in the dataset and writes data to a sqlite3 database for memory optimization.
+    The Sqlite3 database has 3 columns: lemma, pos, and frequency. (lemma, pos) is a key to enable creating unique entries of lemma+PoS
+    combinations while iterating frequency counter if found.
+
+    Args:
+        nlp_size: "large" | "small". Defines which sized model to use in spaCy data preprocessing steps
+        cores_to_use: Specifies how many cores to use for spaCy processing
+        chunk_size: Specifies how large each chunk of the dataset will be loaded in at one time and processsed by spaCy
+
+    Returns:
+        DataFrame: Pandas DataFrame reference to sqlite3 database
+    """
+    # extract each of the rows from the spanish dataset, lemmatize and PoS tag it,
+    # then write that lemma into a csv with lemma as one column, PoS tag as another
+    # column and frequency as the third. Check if a combination of the lemma + PoS exist,
+    # +1 on the frequency, otherwise create a new entry
+
+    root_dir = "data/1-raw/"
+    target_dir = "data/2-extracted/"
+    db_dir = os.path.join(target_dir, "spanish.db")
+
+    if nlp_size == "large":
+
+        try:
+            nlp = spacy.load("es_dep_news_trf")
+        except:
+            from spacy.cli import download
+
+            download("es_dep_news_trf")
+
+            nlp = spacy.load("es_dep_news_trf")
+
+    elif nlp_size == "small":
+        try:
+            nlp = spacy.load("es_core_news_sm")
+        except:
+            from spacy.cli import download
+
+            download("es_core_news_sm")
+
+            nlp = spacy.load("es_core_news_sm")
+    else:
+        raise ValueError(f'nlp_size must be "large" or "small". Got {nlp_size}')
+
+    if os.path.exists(db_dir):
+        os.remove(db_dir)
+
+    con = sqlite3.connect(db_dir)
+    cursor = con.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS word_freq (
+        lemma TEXT,
+        pos TEXT,
+        frequency INTEGER,
+        PRIMARY KEY (lemma, pos)
+    )
+    """
+    )
+    con.commit()
+
+    for batch_i, batch in enumerate(
+        pd.read_csv(os.path.join(root_dir, "spanish.csv"), chunksize=chunk_size)
+    ):
+        # load in chunks as not to explode my PC
+        print(f"Processing Batch {batch_i + 1}")
+        dialogues = batch["dialogue"]
+
+        # for dialogue_i, dialogue in enumerate(batch["dialogue"]):
+        # print(f"Processing Dialogue {dialogue_i + 1} in batch {batch_i + 1}")
+        word_freq_dict = {}
+
+        for dialogue in dialogues:
+            dialogue = dialogue.replace("\\n", " ")
+
+        processed_dialogue = nlp.pipe(
+            dialogues, batch_size=chunk_size, n_process=cores_to_use
+        )
+        for doc in processed_dialogue:
+            for token in doc:
+                if not token.is_punct and not token.is_space:
+                    key = (token.lemma_.lower(), token.pos_)
+                    word_freq_dict[key] = word_freq_dict.get(key, 0) + 1
+
+        for (lemma, pos), freq in word_freq_dict.items():
+            # write to DB, either new entry if unique or +1 otherwise
+            cursor.execute(
+                """
+                INSERT INTO word_freq (lemma, pos, frequency)
+                VALUES (?, ?, ?)
+                ON CONFLICT (lemma, pos)
+                DO UPDATE SET frequency = frequency + ?
+            """,
+                (lemma, pos, freq, freq),
+            )
+
+        con.commit()
+
+    df = pd.read_sql_query("SELECT * FROM word_freq", con)
+
+    con.close()
+    return df
+
+
+def finalize_dataset(limit=5000):
+    """Converts list of lemmas to .csv format, ranks by frequency and limits dataset.
+
+    Args:
+        limit: Limits final dataset to top N frequent lemmas.
+
+    Returns:
+        DataFrame: Final Dataset
+    """
+
+    if type(limit) is not int:
+        raise TypeError(f"limit not of type int. Got {type(limit)}.")
+
+    root_dir = "data/2-extracted/"
+    target_dir = "data/3-final/"
+    db_dir = os.path.join(root_dir, "spanish.db")
+
+    con = sqlite3.connect(db_dir)
+    cursor = con.cursor()
+
+    df = pd.read_sql_query(
+        "SELECT * FROM word_freq ORDER BY frequency DESC LIMIT ?", con, params=(limit,)
+    )
+
+    df.to_csv(
+        os.path.join(target_dir, f"spanish{limit}.csv"), index=True, index_label="ID"
+    )
+
+    return df
